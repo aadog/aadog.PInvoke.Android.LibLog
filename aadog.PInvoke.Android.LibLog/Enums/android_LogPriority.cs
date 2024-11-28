211e8f9f774af1e0a9d7567acce824f10976f45c
qt <g.com>
2024-11-28 23:52:28 +0800

    添加项目文件。

---
+++ aadog.PInvoke.Android.LibLog/Enums/android_LogPriority.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aadog.PInvoke.Android.LibLog.Enums
+{
+    public enum android_LogPriority
+    {
+        ANDROID_LOG_UNKNOWN = 0,
+        ANDROID_LOG_DEFAULT,    /* only for SetMinPriority() */
+        ANDROID_LOG_VERBOSE,
+        ANDROID_LOG_DEBUG,
+        ANDROID_LOG_INFO,
+        ANDROID_LOG_WARN,
+        ANDROID_LOG_ERROR,
+        ANDROID_LOG_FATAL,
+        ANDROID_LOG_SILENT,     /* only for SetMinPriority(); must be last */
+    }
+}
---
+++ aadog.PInvoke.Android.LibLog/LibLog.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using aadog.PInvoke.Android.LibLog.Enums;
+
+namespace aadog.PInvoke.Android.LibLog
+{
+    public partial struct LibLog
+    {
+        [LibraryImport("log")]
+        public static partial int __android_log_print(android_LogPriority prio, [MarshalAs(UnmanagedType.LPUTF8Str)] string tag, [MarshalAs(UnmanagedType.LPUTF8Str)] string fmt);
+
+        public static void Log(android_LogPriority priority, string tag, string msg)
+        {
+            __android_log_print(priority, tag, msg);
+        }
+
+        public static void LogInfo(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_INFO, tag, msg);
+        public static void LogDebug(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_DEBUG, tag, msg);
+        public static void LogError(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_ERROR, tag, msg);
+        public static void LogFatal(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_FATAL, tag, msg);
+        public static void LogWarn(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_WARN, tag, msg);
+        public static void LogVerbose(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_VERBOSE, tag, msg);
+
+        public static void LOG_INFO(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_INFO, tag, msg);
+        public static void LOG_DEBUG(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_DEBUG, tag, msg);
+        public static void LOG_ERROR(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_ERROR, tag, msg);
+        public static void LOG_FATAL(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_FATAL, tag, msg);
+        public static void LOG_WARN(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_WARN, tag, msg);
+        public static void LOG_VERBOSE(string tag, string msg) => Log(android_LogPriority.ANDROID_LOG_VERBOSE, tag, msg);
+    }
+}
